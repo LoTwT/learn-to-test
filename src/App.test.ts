cf97e888ae9a22451c6c011d6dcde0b4c9cbe315
LoTwT <709937065@qq.com>
2023-02-01 14:46:58 +0800

    feat: React Testing Library setup

--- src/App.test.ts
+++
@@ -1,6 +0,0 @@
-// App.test.ts
-import React from "react"
-
-test("test", () => {
-  expect(1 + 1).toBe(2)
-})
---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react"
+import App from "./App"
+
+describe("test", () => {
+  test("first unit test", () => {
+    render(<App />)
+    expect(screen.getByText("Vite + React")).toBeInTheDocument()
+  })
+})
